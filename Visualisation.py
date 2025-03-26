285f535bf3ce6f2769dd5fac85c97f5344359f81
chisphung <phungminhchi2005@gmail.com>
2025-03-27 01:20:29 +0700

    clean code

--- Visualisation.py
+++ Visualisation.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd 
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
