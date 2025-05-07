7dc3e5d6428f6877317cc99b5fcf02eabb804676
phungminhchi <45690401+chisphung@users.noreply.github.com>
2025-05-07 17:38:18 +0700

    Update model to KNN

--- Visualisation.py
+++ Visualisation.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
 df = pd.read_excel('crypto_survey.xlsx')
@@ -26,10 +27,11 @@ y = df_copy['Will_invest'] # Target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 X_train['Invested'] = X_train['Invested'].astype(int)
 X_test['Invested'] = X_test['Invested'].astype(int)
-model = RandomForestClassifier()
+model = KNeighborsClassifier()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+acu = accuracy_score(y_test, y_pred)
 # print("Accuracy:", accuracy_score(y_test, y_pred))
 
 st.title("Dự đoán khả năng đầu tư vào tiền mã hóa")
